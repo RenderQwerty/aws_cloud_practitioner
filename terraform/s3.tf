a1ab050bc1da3c1d985fb320fee57c11053b589f
Yurii Fisakov <fisakov.root@gmail.com>
2019-05-02 12:50:15 +0300

    add object to bucket

--- terraform/s3.tf
+++ terraform/s3.tf
@@ -1,8 +1,16 @@
 resource "aws_s3_bucket" "renderqwerty" {
-  bucket = "renderqwerty"
+  bucket = "${var.s3_bucket_name}"
   acl    = "private"
 
   tags = {
     Name = "JBOF"
   }
+}
+
+resource "aws_s3_bucket_object" "object" {
+  bucket        = "${var.s3_bucket_name}"
+  source        = "${var.s3_object_test}"
+  storage_class = "INTELLIGENT_TIERING"
+  key           = "test.txt"
+  acl           = "public-read"
 }
--- terraform/variables.tf
+++ terraform/variables.tf
@@ -5,3 +5,13 @@ variable aws_access_key {
 variable aws_secret_key {
   description = "AWS Secret Key"
 }
+
+variable s3_bucket_name {
+  description = "Default bucket"
+  default     = "renderqwerty"
+}
+
+variable s3_object_test {
+  description = "test image for s3 bucket"
+  default     = "files/test.txt"
+}
