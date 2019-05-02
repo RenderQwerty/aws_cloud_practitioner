e7ba3577409054aa6a5c8eddfeb23fa1d9d52e41
Yurii Fisakov <fisakov.root@gmail.com>
2019-05-02 10:39:02 +0300

    init resources

---
+++ terraform/main.tf
@@ -0,0 +1,12 @@
+provider "aws" {
+  access_key = "${var.aws_access_key}"
+  secret_key = "${var.aws_secret_key}"
+  region     = "us-east-1"
+}
+
+resource "aws_iam_account_password_policy" "strict" {
+    minimum_password_length = 8
+    require_lowercase_characters = true
+    require_numbers = true
+    allow_users_to_change_password = true
+}
