156c46a12bbdee5a1ed23f3aa6b1eb17427dfbf8
Yurii Fisakov <fisakov.root@gmail.com>
2019-05-06 09:55:42 +0300

    move iam config to separate file

--- terraform/main.tf
+++ terraform/main.tf
@@ -3,10 +3,3 @@ provider "aws" {
   secret_key = "${var.aws_secret_key}"
   region     = "${var.region}"
 }
-
-resource "aws_iam_account_password_policy" "strict" {
-  minimum_password_length        = 8
-  require_lowercase_characters   = true
-  require_numbers                = true
-  allow_users_to_change_password = true
-}
